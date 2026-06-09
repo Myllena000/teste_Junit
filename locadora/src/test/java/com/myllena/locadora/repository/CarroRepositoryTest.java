9c25dca8b0e92c61f1f5973025e326c1211bf85c
Myllena000 <myllena000@hotmail.com>
2026-06-09 00:59:00 -0300

    teste utilizado sql nativo

--- locadora/src/test/java/com/myllena/locadora/repository/CarroRepositoryTest.java
+++ locadora/src/test/java/com/myllena/locadora/repository/CarroRepositoryTest.java
@@ -1,12 +1,16 @@
 package com.myllena.locadora.repository;
 
 import com.myllena.locadora.entity.CarroEntity;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.jdbc.Sql;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.*;
 
 @DataJpaTest
 @ActiveProfiles("teste")
@@ -16,10 +20,23 @@ class CarroRepositoryTest {
     private CarroRepository repository;
 
     @Test
-    void saveCar(){
+    void saveCar() {
         var carro = new CarroEntity("Sedan", 200);
         repository.save(carro);
 
         assertNotNull(carro.getId());
+    }
+
+    // COM SQL
+    @Test
+    @Sql("/sql/carros.sql")
+    void listarModelo() {
+        List<CarroEntity> lista = repository.findByModelo("SUV");
+
+        var carro = lista.stream().findFirst().get();
+
+        assertEquals(1, lista.size());
+        Assertions.assertThat(carro.getModelo()).isEqualTo("SUV");
+
     }
 }
---
+++ locadora/src/test/resources/sql/carros.sql
@@ -0,0 +1,2 @@
+INSERT INTO carro (id, valor_diaria, modelo) VALUES (1, 170.0, 'SUV');
+INSERT INTO carro (id, valor_diaria, modelo) VALUES (2, 130.0, 'HB20');
