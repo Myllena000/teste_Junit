ded261b7c0d5cc080fa7c15f9e3f047d8c9b3d68
Myllena000 <myllena000@hotmail.com>
2026-06-23 01:02:35 -0300

    fazendo a camada Controller para teste

---
+++ locadora/src/main/java/com/myllena/locadora/controller/CarroController.java
@@ -0,0 +1,72 @@
+package com.myllena.locadora.controller;
+
+import com.myllena.locadora.entity.CarroEntity;
+import com.myllena.locadora.exception.EntityNotFoundException;
+import com.myllena.locadora.service.CarroService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+
+@RestController
+@RequestMapping("carros")
+public class CarroController {
+
+    final CarroService service;
+
+    public CarroController(CarroService service) {
+        this.service = service;
+    }
+
+    @PostMapping
+    public ResponseEntity<Object> salvar(@RequestBody CarroEntity entity) {
+        try {
+            CarroEntity carroSalvo = service.salvarCarro(entity);
+            return ResponseEntity.status(HttpStatus.CREATED).body(carroSalvo);
+
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity
+                    .status(HttpStatus.UNPROCESSABLE_ENTITY)
+                    .body(e.getMessage());
+        }
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<CarroEntity> buscarPorID(@PathVariable Long id) {
+        try {
+            service.buscarPorId(id);
+
+        } catch (Exception e) {
+            ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+        }
+        return ResponseEntity.status(HttpStatus.OK).build();
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<CarroEntity> alterarCarro(@PathVariable Long id, @RequestBody CarroEntity entity) {
+        try {
+            service.alterarCarro(id, entity);
+
+        } catch (EntityNotFoundException e) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, e.getMessage());
+        }
+        return ResponseEntity.status(HttpStatus.OK).build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletar(@PathVariable Long id) {
+        try {
+            service.deletarCarro(id);
+
+
+        } catch (EntityNotFoundException e) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, e.getMessage());
+        }
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
+
+
+
+
+}
